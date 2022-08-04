2fb8113b7429378c11870f3418d6a2220af9a803
Ege Seçkin <ege.seckin@outlook.com>
2022-08-04 19:12:19 +0300

    initial commit

---
+++ MovieApp/MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Ege Seçkin on 4.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
